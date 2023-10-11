31eca816c0ef09089e42c14083ca126a67bafb2e
B Karthik Kumar <kumarkarthik.b20@gmail.com>
2023-10-11 06:52:09 +0530

    Commit 003 - Video Status 8/181 Date : 11/10/2023
    In this commit we have used Icons, Axis Alignment, SizedBox, TextStyle

--- Project01-My_Portfolio_App/proj_01_portfolio/lib/main.dart
+++ Project01-My_Portfolio_App/proj_01_portfolio/lib/main.dart
@@ -23,17 +23,150 @@ class Home extends StatelessWidget {
             Row(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.only(right: 50.0),
+                  padding: const EdgeInsets.only(right: 2.0),
                   child: CircleAvatar(
                     radius: 50,
                     backgroundColor: Colors.yellowAccent,
                   ),
                 ),
+                SizedBox(
+                  width: 30,
+                ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Balasundaram Karthik Kumar"),
-                    Text("Fullstack Developer")
+                    Text(
+                      "Balasundaram Karthik",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    Text(
+                      "Fullstack Developer",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    )
                   ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.school_sharp,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Education Details",
+                          style: TextStyle(fontSize: 22),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.file_copy_outlined,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Projects Done",
+                          style: TextStyle(fontSize: 22),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.pin_drop,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Prefredd Location",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.email_rounded,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Email",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Phone Number",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Column(
+                children: [
+                  Text(
+                    "About Me",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Created By KSK",
+                  style: TextStyle(fontSize: 15),
                 )
               ],
             )
