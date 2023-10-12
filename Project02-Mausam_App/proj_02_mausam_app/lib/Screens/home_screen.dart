ea9167e7a6a3f1a49540b3d988ab532c6f53048e
B Karthik Kumar <kumarkarthik.b20@gmail.com>
2023-10-12 16:47:18 +0530

    Code update and understood the logic for initState , Dispose,setState

--- Project02-Mausam_App/proj_02_mausam_app/lib/Screens/home_screen.dart
+++ Project02-Mausam_App/proj_02_mausam_app/lib/Screens/home_screen.dart
@@ -8,6 +8,32 @@ class MausamHome extends StatefulWidget {
 }
 
 class _MausamHomeState extends State<MausamHome> {
+  /* 
+  the below function is called when the widget is initlized like when we are coming back to 
+  home page from any other page
+  */
+  @override
+  void initState() {
+    super.initState();
+    print("This is the init state for mausam home screen");
+  }
+
+  int counter = 1;
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    print("Set function of the widget get called");
+    print("$counter");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("Widget Destroyed with counter value $counter");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +44,23 @@ class _MausamHomeState extends State<MausamHome> {
           ),
         ),
       ),
-      body: Text("Home Page"),
+      body: Column(
+        children: [
+          Text(
+            "Home Page",
+          ),
+          FloatingActionButton(
+            onPressed: () => setState(
+              () {
+                counter *= 5;
+              },
+            ),
+          ),
+          Text(
+            '$counter',
+          ),
+        ],
+      ),
     );
   }
 }
